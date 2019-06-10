fd8a3cc0381ed2e6f0dfbe218e9723748cdcc2bc
Tessa Kelly <tessakkelly@gmail.com>
2019-06-10 11:53:24 -0700

    Set up two distinct modal examples

--- examples/Main.elm
+++ examples/Main.elm
@@ -1,7 +1,9 @@
 module Main exposing (main)
 
 import Browser exposing (element)
+import Dict exposing (Dict)
 import Html exposing (..)
+import Html.Attributes
 import Modal
 import Platform
 
@@ -18,55 +20,132 @@ main =
 
 init : {} -> ( Model, Cmd Msg )
 init flags =
-    ( { modal = Modal.init
-      }
+    ( Dict.fromList
+        [ ( 0, Modal.init )
+        , ( 1, Modal.init )
+        , ( 2, Modal.init )
+        ]
     , Cmd.none
     )
 
 
 type alias Model =
-    { modal : Modal.Model
-    }
+    Dict Int Modal.Model
 
 
 type Msg
-    = ModalMsg Modal.Msg
+    = ModalMsg Int Modal.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ModalMsg modalMsg ->
-            let
-                ( newModalState, modalCmd ) =
-                    Modal.update modalMsg model.modal
-            in
-            ( { model | modal = newModalState }
-            , Cmd.map ModalMsg modalCmd
-            )
+        ModalMsg modalId modalMsg ->
+            case Dict.get modalId model of
+                Just modal ->
+                    let
+                        ( newModalState, modalCmd ) =
+                            Modal.update modalMsg modal
+                    in
+                    ( Dict.insert modalId newModalState model
+                    , Cmd.map (ModalMsg modalId) modalCmd
+                    )
+
+                Nothing ->
+                    ( model, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.map ModalMsg (Modal.subscriptions model.modal)
+    Dict.toList model
+        |> List.map (\( id, modal ) -> Sub.map (ModalMsg id) (Modal.subscriptions modal))
+        |> Sub.batch
 
 
 view : Model -> Html Msg
 view model =
     div
         []
-        [ button [] [ text "Supports focus" ]
-        , Html.map ModalMsg
-            (Modal.view
-                { ifClosed = button [ Modal.openOnClick ] [ text "Launch Modal" ]
-                , ifOpen =
-                    div []
-                        [ text "Modal content"
-                        , button (Modal.closeOnClick :: Modal.singleFocusableElement)
-                            [ text "Close Modal" ]
-                        ]
-                }
-                model.modal
-            )
-        , button [] [ text "Supports focus" ]
+        [ h1 [] [ text "Modal examples" ]
+        , case Dict.get 0 model of
+            Just modal ->
+                section []
+                    [ h2 [] [ text "Single focusable element" ]
+                    , Html.map (ModalMsg 0)
+                        (Modal.view
+                            { ifClosed = button [ Modal.openOnClick ] [ text "Launch Modal" ]
+                            , ifOpen =
+                                div []
+                                    [ text "Modal content"
+                                    , button
+                                        (Modal.closeOnClick
+                                            :: Modal.singleFocusableElement
+                                        )
+                                        [ text "Close Modal" ]
+                                    ]
+                            }
+                            modal
+                        )
+                    ]
+
+            Nothing ->
+                text ""
+        , case Dict.get 1 model of
+            Just modal ->
+                section []
+                    [ h2 [] [ text "Two focusable elements" ]
+                    , Html.map (ModalMsg 1)
+                        (Modal.view
+                            { ifClosed = button [ Modal.openOnClick ] [ text "Launch Modal" ]
+                            , ifOpen =
+                                div []
+                                    [ text "Modal content"
+                                    , button
+                                        (Modal.closeOnClick
+                                            :: Modal.firstFocusableElement
+                                        )
+                                        [ text "Close Modal" ]
+                                    , a
+                                        (Html.Attributes.href "www.noredink.com"
+                                            :: Modal.lastFocusableElement
+                                        )
+                                        [ text "Go to noredink.come" ]
+                                    ]
+                            }
+                            modal
+                        )
+                    ]
+
+            Nothing ->
+                text ""
+        , case Dict.get 2 model of
+            Just modal ->
+                section []
+                    [ h2 [] [ text "Three focusable elements" ]
+                    , Html.map (ModalMsg 2)
+                        (Modal.view
+                            { ifClosed = button [ Modal.openOnClick ] [ text "Launch Modal" ]
+                            , ifOpen =
+                                div []
+                                    [ text "Modal content"
+                                    , a
+                                        (Html.Attributes.href "www.noredink.com"
+                                            :: Modal.firstFocusableElement
+                                        )
+                                        [ text "Go to noredink.come" ]
+                                    , button [ Modal.closeOnClick ]
+                                        [ text "Close Modal" ]
+                                    , a
+                                        (Html.Attributes.href "www.noredink.com"
+                                            :: Modal.lastFocusableElement
+                                        )
+                                        [ text "Go to noredink.come" ]
+                                    ]
+                            }
+                            modal
+                        )
+                    ]
+
+            Nothing ->
+                text ""
         ]
