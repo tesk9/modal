1a8156105f53e1b87f991df36ee53abc32354622
Tessa Kelly <tessakkelly@gmail.com>
2019-08-16 13:47:43 -0700

    Update example

--- src/Accessibility/Modal.elm
+++ src/Accessibility/Modal.elm
@@ -14,31 +14,19 @@ module Accessibility.Modal exposing
 
     view : Html Modal.Msg
     view =
-        Modal.view
-            { overlayColor = "rgba(128, 0, 128, 0.7)"
-            , wrapMsg = identity
-            , autofocusOn = Modal.Default
-            , modalAttributes =
-                [ style "background-color" "white"
-                , style "border-radius" "4px"
-                , style "border" "2px solid purple"
-                , style "margin" "40px auto"
-                , style "padding" "20px"
-                , style "max-width" "600px"
-                , style "min-height" "40vh"
-                ]
-            , title = ( "Intro Modal", [] )
-            , content =
-                \{ onlyFocusableElement } ->
-                    div
-                        [ style "display" "flex"
-                        ]
+        Modal.view identity
+            "Example modal"
+            [ Modal.onlyFocusableElementView
+                (\onlyFocusableElementAttributes ->
+                    div []
                         [ text "Welcome to this modal! I'm so happy to have you here with me."
                         , button
-                            (onClick Modal.close :: onlyFocusableElement)
-                            [ text "Close intro modal" ]
+                            (onClick Modal.close :: onlyFocusableElementAttributes)
+                            [ text "Close Modal" ]
                         ]
-            }
+                )
+            ]
+            modal
 
 @docs Model, init, subscriptions
 @docs Msg, update, close, open
